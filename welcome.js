0fa059f5e7f5f46dbd42cbe6edda6c006b29b7bf
Philip Karanja <kpk.my.mailings@gmail.com>
2024-04-15 14:43:23 -0500

    practicing git push

--- welcome.js
+++ welcome.js
@@ -1,1 +1,14 @@
-console.log("hello class, welcome to module 3 day 2")
+console.log("hello class, welcome to module 3 day 2") //first git push in saturday class
+
+const getTax = () => {
+    const taxRate = 0.08;
+    const tax = price * taxRate;
+    return tax
+};
+//when using multiple lines of code in the function body use the return keyword and the curly braces
+//if only one line then you can ommit return keyword and curly braces
+
+const subtractTwoNumbers = (num1, num2) => num1 - num2;
+console.log(subtractTwoNumbers(9, 2))
+
+
